290b47b0bb882e115b04078c531ce44fa209c033
Stepan <stepan.petrov@planisense.com>
2022-08-12 08:33:12 +0300

    fix: path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'table',
-    pathMatch: 'full'
-  },
-  {
-    path: 'table',
     loadChildren: () => import('./modules/table/table.module').then(m => m.TableModule),
   },
 ];
